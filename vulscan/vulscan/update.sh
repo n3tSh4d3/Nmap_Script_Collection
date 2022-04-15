9dd169091ba036367934e88d04d4773e074e6d00
n3tSh4d3 <adriano.condro@gmail.com>
2022-04-15 09:49:39 +0200

    Delete vulscan/vulscan directory

--- vulscan/vulscan/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-declare -a databases=("cve" "exploitdb" "openvas" "osvdb" "scipvuldb" "securityfocus" 
-                      "securitytracker" "xforce")
-
-for DB in "${databases[@]}"; do
-    wget https://www.computec.ch/projekte/vulscan/download/${DB}.csv
-done
